6fc1fd6ee403345418a01c793b5260c57a951591
Miguel Palhas <mpalhas@gmail.com>
2015-10-16 17:19:44 +0100

    First Commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'dashing'
+gem 'slim'
+gem 'octokit'
+gem 'dotenv'
+gem 'haml'
+gem 'google-api-client'
+
+## Remove this if you don't need a twitter widget.
+gem 'twitter', '>= 5.9.0'
---
+++ jobs/github_prs.rb
@@ -0,0 +1,21 @@
+require 'octokit'
+
+SCHEDULER.every '1m', :first_in => 0 do |job|
+  client = Octokit::Client.new(:access_token => ENV["GITHUB_AUTH_TOKEN"])
+  my_organization = ENV["GITHUB_ORG_NAME"]
+  repos = client.organization_repositories(my_organization).map { |repo| repo.name }
+
+  open_pull_requests = repos.inject([]) { |pulls, repo|
+    client.pull_requests("#{my_organization}/#{repo}", :state => 'open').each do |pull|
+      pulls.push({
+        title: pull.title,
+        repo: repo,
+        updated_at: pull.updated_at.strftime("%b %-d %Y, %l:%m %p"),
+        creator: "@" + pull.user.login,
+        })
+    end
+    pulls
+  }
+
+  send_event('github', { header: "Github Pull Requests", pulls: open_pull_requests })
+end
